638a079322a783a60c8283d086c5d9ce84299b69
Simin Cao <simin.cao@outlook.com>
2021-04-29 20:17:06 +0200

    #7 initial implement pokemon image, only display official artwork

---
+++ src/components/PokemonImage.vue
@@ -0,0 +1,22 @@
+<template>
+    <img v-if="images"
+         v-lazy="{ src: images.other['official-artwork'].front_default, 
+                    loading: 'your loading image url', 
+                    error: 'your error image url' }" 
+          class="ml-auto mr-auto h-80 w-80 bg-gray-200 mt-10 rounded-md"
+    />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'PokemonImage',
+    props: {
+      images: {
+        type: Object,
+        required: true
+      }
+    },
+
+  })
+</script>
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,23 @@ import App from './App.vue'
 import './index.css'
 import { store, key } from './store'
 import filters from './helpers/Filters'
+import VueLazyLoad from 'vue3-lazyload'
 
 const app = createApp(App);
 app.use(VueAxios, axios);
 app.use(store, key)
 app.config.globalProperties.$filters=filters;
+app.use(VueLazyLoad, {
+    loading: () => {
+        console.log('loading')
+      },
+      error: () => {
+        console.log('error')
+      },
+      loaded: () => {
+        console.log('loaded')
+      }
+  })
 app.mount('#app');
 
 
