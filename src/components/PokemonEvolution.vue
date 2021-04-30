8992cb5425947270c9dfd59c7e96114b20aab046
Simin Cao <simin.cao@outlook.com>
2021-04-30 14:28:31 +0200

    #9 fix bug for eevee evolution tree

--- src/components/PokemonEvolution.vue
+++ src/components/PokemonEvolution.vue
@@ -3,16 +3,16 @@
     <i class="fas fa-sync-alt text-green-500"></i>
     <span>Evolution</span>
   </div>
-  <div class="grid grid-cols-3">
+  <div class="grid grid-cols-3 h-52 overflow-y-scroll">
     <div v-for="pokemon in pokemonEvolutionChain" :key="pokemon.name" class="text-center my-2">
-        <img 
-        :class="pokemon.isSelf ? 'border-4 border-green-400' : 'border-4'"
-        class="h-16 w-16 rounded-full mx-auto"
-            v-lazy="{ src: pokemon.imageUrl, 
-                    loading: 'your loading image url', 
-                    error: 'your error image url' }" 
-            alt="">
-        <span class="text-sm">{{$filters.capitalizeFirstCharacter(pokemon.name)}}</span>
+      <img 
+      :class="pokemon.isSelf ? 'border-4 border-green-400' : 'border-4'"
+      class="h-12 w-12 rounded-full mx-auto"
+          v-lazy="{ src: pokemon.imageUrl, 
+                  loading: 'https://via.placeholder.com/150', 
+                  error: 'your error image url' }" 
+          alt="">
+      <span class="text-sm">{{$filters.capitalizeFirstCharacter(pokemon.name)}}</span>
     </div>
   </div>
 </template>
@@ -50,19 +50,28 @@
       try {
         const response = await this.axios.get(this.pokemonEvolutionChainUrl);
         let data=response.data.chain;
-        do {
-            this.pokemonEvolutionChain.push({
+        this.pushToPokemonEvolutionChainArray(data);
+        data=data.evolves_to;
+        data.forEach((firstRevolutions:any) => {
+          this.pushToPokemonEvolutionChainArray(firstRevolutions);
+          firstRevolutions=firstRevolutions.evolves_to;
+          firstRevolutions.forEach((secondRevolution:any) => {
+            this.pushToPokemonEvolutionChainArray(secondRevolution);
+          });
+        });
+      } catch (error) {
+        console.error(error);
+      }  
+    },
+    methods: {
+      pushToPokemonEvolutionChainArray(data:any){
+        this.pokemonEvolutionChain.push({
               name:data.species.name,
               spieciesUrl:data.species.url,
               imageUrl:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.species.url.split("/").slice(-2)[0]}.png`,
               isSelf: (data.species.url===this.speciesUrl?true:false)
-            });
-            data=data.evolves_to[0];
-        } while (data);
-        console.log(this.pokemonEvolutionChain);
-      } catch (error) {
-        console.error(error);
-      }  
+        });
+      }
     }
   })
 </script>
