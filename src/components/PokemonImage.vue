a0de9d29f2a4d4c48b883df813d1c5c78f1192ec
Simin Cao <simin.cao@outlook.com>
2021-04-30 12:25:11 +0200

    #7 load default image if official-artwork absent

--- src/components/PokemonImage.vue
+++ src/components/PokemonImage.vue
@@ -1,10 +1,11 @@
 <template>
     <img v-if="images"
-         v-lazy="{ src: images.other['official-artwork'].front_default, 
+         v-lazy="{ src: images.other['official-artwork'].front_default?
+         images.other['official-artwork'].front_default:
+         images.front_default, 
                     loading: 'your loading image url', 
                     error: 'your error image url' }" 
-          class="h-48 w-48 mx-auto"
-    />
+          class="h-48 w-48 mx-auto"/>
 </template>
 
 <script lang="ts">
