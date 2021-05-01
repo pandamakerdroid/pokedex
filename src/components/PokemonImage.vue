82a3dfd86328ac9f3c3fb09200c43893671d5812
Simin Cao <simin.cao@outlook.com>
2021-05-01 15:56:10 +0200

    #7 add fa icon spinner for image loading

--- src/components/PokemonImage.vue
+++ src/components/PokemonImage.vue
@@ -1,11 +1,11 @@
 <template>
-    <img v-if="images"
-         v-lazy="{ src: images.other['official-artwork'].front_default?
-         images.other['official-artwork'].front_default:
-         images.front_default, 
-                    loading: 'https://craftypixels.com/placeholder-image/100x100/fafafa/fafafa',
-                    error: 'your error image url' }" 
-          class="h-48 w-48 mx-auto rounded"/>
+    <div v-if="!isLoad"
+          class="bg-gray-100 h-48 w-48 mx-auto rounded flex justify-center items-center">
+          <i class="fas fa-fan animate-spin text-9xl text-gray-300"></i>
+    </div>
+    <img v-bind:class="{ 'hidden': !isLoad}" 
+          :src="avatarUrl" @load="loaded"
+          class="h-48 w-48 mx-auto bg-gray-100 rounded"/>
 </template>
 
 <script lang="ts">
@@ -18,6 +18,28 @@
         required: true
       }
     },
-
+    watch: { 
+      images(newImages, oldImages) { 
+        if (!newImages || newImages.front_default===oldImages.front_default) { return };
+        this.avatarUrl = this.setAvatarUrl(newImages);
+      }
+    },
+    data() {
+      return{
+        isLoad:false,
+        avatarUrl:this.setAvatarUrl(this.images)
+      }
+    },
+    methods: {
+      setAvatarUrl(images:any){
+        this.isLoad=false;
+        return images.other['official-artwork'].front_default?
+          images.other['official-artwork'].front_default:
+          images.front_default;
+      },
+      loaded(){
+        this.isLoad=true;
+      }
+    }
   })
 </script>
